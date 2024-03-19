e333681667484721163e04b575d2e407e54116a7
smdthiranjaya <s.m.d.thiranjaya@gmail.com>
2024-03-20 00:09:53 +0530

    Adding jsonwebtoken packge

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
