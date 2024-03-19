0320705308945477b5fc5ea4208aa35b6c73cb8f
smdthiranjaya <s.m.d.thiranjaya@gmail.com>
2024-03-19 16:34:07 +0530

    Login endpoint updated

--- index.js
+++ index.js
@@ -5,27 +5,19 @@ const app = express();
 app.use(express.json());
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Heroku sets this environment variable
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false // Note: only use this for Heroku's free tier
   }
 });
 
 app.post('/login', async (req, res) => {
-  const { accessToken } = req.body;
+  const { email, name, pictureURL, auth0Id } = req.body; // Adjust according to the actual data sent from your client
   try {
-    const result = await pool.query('INSERT INTO users(accessToken) VALUES($1) RETURNING *', [accessToken]);
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-app.post('/users', async (req, res) => {
-  const { accessToken } = req.body;
-  try {
-    const result = await pool.query('INSERT INTO users(accessToken) VALUES($1) RETURNING *', [accessToken]);
+    const result = await pool.query(
+      'INSERT INTO users(email, name, picture_url, auth0_id) VALUES($1, $2, $3, $4) RETURNING *',
+      [email, name, pictureURL, auth0Id]
+    );
     res.json(result.rows[0]);
   } catch (err) {
     console.error(err);
